6f030fbe9f3ca1ed188e6683f3ca279967583801
Théo G <101090069+Theo-740@users.noreply.github.com>
2024-05-06 09:22:42 +0200

    added range to cursor

--- smartshare/src/client/client.rs
+++ smartshare/src/client/client.rs
@@ -284,18 +284,21 @@ impl Client {
         }
     }
 
-    async fn on_ide_cursor_move(&mut self, offset: u64) {
+    async fn on_ide_cursor_move(&mut self, offset: u64, range: u64) {
         let _ = self
             .server
             .send(MessageServer::Cursor {
                 id: self.client_id,
                 offset,
+                range,
             })
             .await;
     }
 
-    async fn on_server_cursor_move(&mut self, id: usize, offset: u64) {
-        self.ide.send(MessageIde::Cursor { id, offset }).await;
+    async fn on_server_cursor_move(&mut self, id: usize, offset: u64, range: u64) {
+        self.ide
+            .send(MessageIde::Cursor { id, offset, range })
+            .await;
     }
 
     pub async fn on_message_server(&mut self, message: MessageServer) {
@@ -305,7 +308,9 @@ impl Client {
             MessageServer::Error { error: err } => self.on_server_error(err).await,
             MessageServer::RequestFile => self.on_request_file().await,
             MessageServer::File { file, version } => self.on_receive_file(file, version).await,
-            MessageServer::Cursor { id, offset } => self.on_server_cursor_move(id, offset).await,
+            MessageServer::Cursor { id, offset, range } => {
+                self.on_server_cursor_move(id, offset, range).await
+            }
         }
     }
 
@@ -317,7 +322,7 @@ impl Client {
             MessageIde::RequestFile => warn!("IDE sent RequestFile"),
             MessageIde::Error { .. } => warn!("IDE sent error"),
             MessageIde::Ack => self.on_ide_ack().await,
-            MessageIde::Cursor { offset, .. } => self.on_ide_cursor_move(offset).await,
+            MessageIde::Cursor { offset, range,.. } => self.on_ide_cursor_move(offset, range).await,
         }
     }
 }
